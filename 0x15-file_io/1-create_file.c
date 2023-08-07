e24264e0886bdcd0793c8ad695d31ebba601ec0b
Justin <orangijdavis427@gmail.com>
2023-08-07 05:41:29 -0700

    wueh amend

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,8 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (let = 0; text_content[let]; let++);
+	for (let = 0; text_content[let]; let++)
+		;
 
 	k = write(fs, text_content, let);
 
